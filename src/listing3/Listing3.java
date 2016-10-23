58e258b5771d6f097479fb4ab356b738afcdd352
sazzad <sazzad@DESKTOP-304IF3P>
2016-10-23 17:06:27 +0600

    this is first commit

--- src/listing3/Listing3.java
+++ src/listing3/Listing3.java
@@ -16,6 +16,8 @@ public class Listing3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("hello ");
     }
     
 }
